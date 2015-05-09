400484bae2607ec44ad76f9b9da84f36f5713faa
jdgregorian <jdgregorian@seznam.cz>
2015-05-09 16:24:02 +0200

    Literature and some results to Info added

--- src/class/StumpForest.m
+++ src/class/StumpForest.m
@@ -5,25 +5,34 @@ classdef StumpForest
         nvars  % # of features
         NTrees % number of trees
         performances % array of performances
+        FBoot % fraction of input data used to training
     end
     
 methods
-  function SF = StumpForest(data, labels, NTrees)     
+  function SF = StumpForest(data, labels, NTrees, FBoot)     
 
     % initialize
     SF.NTrees = 0;
     SF.nvars=size(data,2);
+    if nargin < 4
+      ST.FBoot = 1;
+    else
+      ST.FBoot = FBoot;
+    end
+    datacount = size(data,1);
+    datause = ceil(ST.FBoot*datacount);
 
     for T = 1:NTrees
       % bootstrap
-      useInd = randi(size(data,1),1,size(data,1));
-%       Tree = StumpTree(data(useInd,:),labels);
-      Tree = LinearTree(data(useInd,:),labels);
+      useInd = randi(datause,1,datause);
+      labeluse = labels(useInd);
+%       Tree = StumpTree(data(useInd,:),labeluse);
+      Tree = LinearTree(data(useInd,:),labeluse);
       y = Tree.predict(data(useInd,:));
       SF.Trees{T} = Tree;
       SF.NTrees = SF.NTrees + 1;
       % stump performance counting
-      SF.performances(T) = sum((y'==labels))/length(labels);
+      SF.performances(T) = sum((y'==labeluse))/length(labeluse);
     end
   end    
     
