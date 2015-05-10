cdc244a51723d4280b2d1fe94cc831738dcfab5f
jdgregorian <jdgregorian@seznam.cz>
2015-05-10 23:53:06 +0200

    LinearTree training written

--- src/class/LinearTree.m
+++ src/class/LinearTree.m
@@ -10,6 +10,7 @@ classdef LinearTree
     traindata % data used for tree training
 
     % Tree properties
+    Nodes    % # of nodes
     children  % children coordinates
     parent    % parent coordinates
     splitZero % 'zero' points determining the split boundary
@@ -29,8 +30,9 @@ methods
     ST.onescount = Nsubjects - ST.zerocount;
     ST.traindata = data;
     
-    ST.children = [0 0];
+    ST.Nodes = 1;
     ST.parent = 0;
+    ST.children = [0 0];
     ST.splitZero = NaN(1,ST.features);
     ST.splitOne = NaN(1,ST.features);
     ST.nodeData = ones(1,Nsubjects);
@@ -39,30 +41,47 @@ methods
       fprintf('Data length differs from labels length!');
       return
     end
-     
+    
     nPureLeaf = true; % leaves which are not pure or split nodes
+    nNodes = 1; % number of nodes
     i = 0;
+    % training cycle start
     while any(nPureLeaf) && i < 10
       i = i+1; % just to be sure this ends
+      
+      % training drawing for 2D
+      if ST.features == 2
+        figure(i)
+        scatter(data(~logical(labels),1),data(~logical(labels),2),'o','blue')
+        hold on
+        scatter(data(logical(labels),1),data(logical(labels),2),'s','green')
+        for p = 1:Nsubjects
+          text(data(p,1)+0.05,data(p,2),num2str(p),'FontSize',7)
+        end
+        xlim manual
+        ylim manual
+      end
       
       leafInd = find(nPureLeaf);
       nToSplit = sum(nPureLeaf); % number of leaves possible to split
       I = zeros(1,nToSplit);
       splitZ = NaN(nToSplit,ST.features);
       splitO = NaN(nToSplit,ST.features);
-      dataIndZ = {};
-      dataIndO = {};
+      dataIndZ = cell(nToSplit,1);
+      dataIndO = cell(nToSplit,1);
+      actualDataInd = true(nToSplit,Nsubjects);
       for s = 1:nToSplit
         actualDataInd(s,:) = (ST.nodeData == leafInd(s));
         [I(s),splitZ(s,:),splitO(s,:),dataIndZ{s},dataIndO{s}] = ...
-          LinearTree.splitGain(data(actualDataInd(s,:),:),actualDataInd(s,:),labels);
+          LinearTree.splitGain(data,actualDataInd(s,:),labels);
       end
       
       % split node with the maximum information gain
       [~,maxInd] = max(I);
       
       ST.parent(end+1:end+2) = leafInd(maxInd);
-      nNodes = length(ST.parent);
+      nNodes = nNodes + 2;
+      ST.Nodes = nNodes;
       ST.children(leafInd(maxInd),:) = [nNodes-1 nNodes];
       ST.children(nNodes-1:nNodes,:) = zeros(2,2);
       
@@ -71,30 +90,45 @@ methods
       ST.splitZero(nNodes-1:nNodes,:) = NaN(2,ST.features);
       ST.splitOne(nNodes-1:nNodes,:) = NaN(2,ST.features);
       
-      changeIndZ = actualDataInd(maxInd,:);
-      changeIndZ(dataIndZ{maxInd}) = false;
-      changeIndO = actualDataInd(maxInd,:);
-      changeIndO(dataIndO{maxInd}) = false;
+      changeIndZ = false(Nsubjects,1); % actualDataInd(maxInd,:);
+      changeIndZ(dataIndZ{maxInd}) = true;
+      changeIndO = false(Nsubjects,1); % actualDataInd(maxInd,:);
+      changeIndO(dataIndO{maxInd}) = true;
       ST.nodeData(changeIndZ) = nNodes - 1;
       ST.nodeData(changeIndO) = nNodes;
       
-      nPureLeaf(maxInd) = 0; % leaf became splitting node
+      nPureLeaf(leafInd(maxInd)) = 0; % leaf became splitting node
       nPureLeaf(nNodes-1) = ~all(~labels(dataIndZ{maxInd}));
       nPureLeaf(nNodes) = ~all(labels(dataIndO{maxInd}));
+      
+      % training split drawing in 2D
+      if ST.features == 2
+        sZ = splitZ(maxInd,:);
+        sO = splitO(maxInd,:);
+        scatter(sZ(1),sZ(2),'x','blue')
+        scatter(sO(1),sO(2),'x','green')
+
+        x(i,1) = min(data(actualDataInd(maxInd,:),1));
+        x(i,2) = max(data(actualDataInd(maxInd,:),1));
+        ybound(i,1) = ((sZ(1)-x(i,1))^2-(sO(1)-x(i,1))^2+sZ(2)^2-sO(2)^2)/(2*(sZ(2)-sO(2)));
+        ybound(i,2) = ((sZ(1)-x(i,2))^2-(sO(1)-x(i,2))^2+sZ(2)^2-sO(2)^2)/(2*(sZ(2)-sO(2)));
+        for l = 1:i
+          plot(x(l,:),ybound(l,:),'r')
+        end
+        hold off
+      end
     end
-    % class division
-    A = data(~logical(labels),:);
-    B = data(logical(labels),:);
-    
-    % count mean
-    ST.splitZero = mean(A,1);
-    ST.splitOne = mean(B,1);
-    
+    % training cycle end
   end    
     
   function y = predict(ST, data)
     nData = size(data,1);
     y = zeros(nData,1);
+    
+    for d = 1:nData
+      zeroDist = LinearTree.distance(repmat(ST.splitZero,nData,1),repmat(data,ST.Nodes,1));
+      oneDist = LinearTree.distance(repmat(ST.splitOne,nData,1),repmat(data,ST.Nodes,1));
+    end
     zeroDist = sqrt(sum((data-repmat(ST.splitZero,nData,1)).^2,2));
     oneDist = sqrt(sum((data-repmat(ST.splitOne,nData,1)).^2,2));
     y(oneDist < zeroDist) = 1; 
@@ -104,25 +138,28 @@ end
 
 methods (Static)
   
-  % TODO: splitGain
   function [I,splitZero,splitOne,dataIndZ,dataIndO] = splitGain(data,index,labels)
-    
-    nData = size(data,1);
-    dataID = 1:nData;
+  % splitGain returns information value I of the split determined with 
+  % points splitZero and splitOne. Furthermore, it returns apropriate 
+  % indices of data: dataIndZ and dataIndO
+  
+    actualData = data(index,:);
+    nData = size(actualData,1);
+    dataID = find(index);
     
     % class division
-    A = data(~logical(labels(index)),:);
-    B = data(logical(labels(index)),:);
+    A = data(~logical(labels) & index,:);
+    B = data(logical(labels) & index,:);
     
     % count mean
     splitZero = mean(A,1);
     splitOne = mean(B,1);
     
     % count indexes
-    zeroDist = sqrt(sum((data-repmat(splitZero,nData,1)).^2,2));
-    oneDist = sqrt(sum((data-repmat(splitOne,nData,1)).^2,2));
+    zeroDist = LinearTree.distance(actualData,repmat(splitZero,nData,1));
+    oneDist = LinearTree.distance(actualData,repmat(splitOne,nData,1));
     dataIndZ = dataID(zeroDist<oneDist);
-    dataIndO = dataID(zeroDist>=oneDist);
+    dataIndO = dataID(zeroDist>=oneDist); 
     
     % count information gain
     I = LinearTree.infoGainSet(dataIndZ,dataIndO,labels);
@@ -155,6 +192,11 @@ methods (Static)
   % p is matrix of probabilities
     H = - sum(p.*log(p),2);
     H(isnan(H)) = 0;
+  end
+  
+  function D = distance(a,b)
+  % count distance between two points or two sets of points
+    D = sqrt(sum((a-b).^2,2)); 
   end
   
 end
--- test/testLinearTree.m
+++ test/testLinearTree.m
@@ -4,26 +4,32 @@ close all
 % data
 nZeros = 10;
 nOnes = 10;
+nTest = 5;
 dim = 2;
 
 A = randn(nZeros,dim);
-B = randn(nZeros,dim) + 1;
+B = randn(nOnes,dim) + 1;
 labels = [ones(1,nZeros),zeros(1,nOnes)];
+
+C = randn(nTest,dim) + 0.5;
 
 % tree training
 L = LinearTree([A;B],labels);
 
-% plot results
-figure(1)
-scatter(A(:,1),A(:,2))
-hold on
-scatter(B(:,1),B(:,2),'s')
-scatter(L.splitZero(1),L.splitZero(2),'x')
-scatter(L.splitOne(1),L.splitOne(2),'x')
+% tree prediction
+y = L.predict(C);
 
-x(1) = min([A(:,1);B(:,1)]);
-x(2) = max([A(:,1);B(:,1)]);
-ybound(1) = ((L.splitZero(1)-x(1))^2-(L.splitOne(1)-x(1))^2+L.splitZero(2)^2-L.splitOne(2)^2)/(2*(L.splitZero(2)-L.splitOne(2)));
-ybound(2) = ((L.splitZero(1)-x(2))^2-(L.splitOne(1)-x(2))^2+L.splitZero(2)^2-L.splitOne(2)^2)/(2*(L.splitZero(2)-L.splitOne(2)));
-plot(x,ybound,'r')
-hold off
+% plot results
+% figure(1)
+% scatter(A(:,1),A(:,2))
+% hold on
+% scatter(B(:,1),B(:,2),'s')
+% scatter(L.splitZero(1),L.splitZero(2),'x')
+% scatter(L.splitOne(1),L.splitOne(2),'x')
+% 
+% x(1) = min([A(:,1);B(:,1)]);
+% x(2) = max([A(:,1);B(:,1)]);
+% ybound(1) = ((L.splitZero(1)-x(1))^2-(L.splitOne(1)-x(1))^2+L.splitZero(2)^2-L.splitOne(2)^2)/(2*(L.splitZero(2)-L.splitOne(2)));
+% ybound(2) = ((L.splitZero(1)-x(2))^2-(L.splitOne(1)-x(2))^2+L.splitZero(2)^2-L.splitOne(2)^2)/(2*(L.splitZero(2)-L.splitOne(2)));
+% plot(x,ybound,'r')
+% hold off
