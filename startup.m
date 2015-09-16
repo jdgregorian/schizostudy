dadf8b1292b31d330a82bffa6c18f03512ce1ab0
Zbynek Pitra <jdgregorian@seznam.cz>
2015-09-16 12:00:30 +0200

    Startup task info adjusted for sharing

--- startup.m
+++ startup.m
@@ -9,17 +9,6 @@ SCdata = fullfile('data','data_SC_190subjects.mat');
 SCdata_old = fullfile('data','data_SC_203subjects.mat');
 
 % Startup tasks informations
-fprintf('Checkout SVM toolbox.\n')
-
-fprintf('\nMost recent: \n')
-fprintf('    Change names of saved results to lower case\n')
-fprintf('    Rebuild experiment folder structure.\n')
-fprintf('    Solve Bayes methods according to Arbabshirani - meeting\n')
-fprintf('    ANN - good settings\n')
-fprintf('    RBF\n')
-fprintf('    Implement all Arbabshirani''s methods.\n')
-fprintf('    Prepare testing kit for different settings - improve testRFparams?\n')
-fprintf('    Boosting strategies\n')
-fprintf('    Different SVMTree settings\n')
-fprintf('    Implement t-test (kendall) feature selection from MATLAB.\n')
-fprintf('    Error printing in listSettingsResults\n')
+if exist(fullfile('test','sys','.creator'),'file')
+  taskInfo
+end
---
+++ test/sys/taskInfo.m
@@ -0,0 +1,15 @@
+% Startup tasks informations
+fprintf('Checkout SVM toolbox.\n')
+
+fprintf('\nMost recent: \n')
+fprintf('    Change names of saved results to lower case\n')
+fprintf('    Rebuild experiment folder structure.\n')
+fprintf('    Solve Bayes methods according to Arbabshirani - meeting\n')
+fprintf('    ANN - good settings\n')
+fprintf('    RBF\n')
+fprintf('    Implement all Arbabshirani''s methods.\n')
+fprintf('    Prepare testing kit for different settings - improve testRFparams?\n')
+fprintf('    Boosting strategies\n')
+fprintf('    Different SVMTree settings\n')
+fprintf('    Implement t-test (kendall) feature selection from MATLAB.\n')
+fprintf('    Error printing in listSettingsResults\n')
