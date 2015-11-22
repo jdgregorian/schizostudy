519f882b5ce8464a5e004328ea6af45605b6badf
jdgregorian <jdgregorian@seznam.cz>
2015-11-22 21:04:53 +0100

    Experiment one ICA update

--- exp/experiments/exp_arbab_abs_oneICA.m
+++ exp/experiments/exp_arbab_abs_oneICA.m
@@ -1,5 +1,6 @@
 % Experiment for running arbabshirani settings on data with
 % absolute values of coefficients but without running ICASSO
+% plus traintest data with ICASSO
 
 % initialization
 expfolder = fullfile('exp', 'experiments');
@@ -9,14 +10,16 @@ arbabfolder = fullfile('data', 'arbabshirani');
 looTrainData = fullfile(arbabfolder, 'loo', 'adCorrAbs_oneICA', '100subj_training.mat');
 looTestData = fullfile(arbabfolder, 'loo', 'adCorrAbs_oneICA', '80subj_testing.mat');
 looAllData = fullfile(arbabfolder, 'loo', 'adCorrAbs_oneICA', '180subj_all.mat');
-traintestData = fullfile(arbabfolder, 'traintest', 'adCorrAbs_oneICA');
+traintestOneICAData = fullfile(arbabfolder, 'traintest', 'adCorrAbs_oneICA');
+traintestAbsData = fullfile(arbabfolder, 'traintest', 'adCorrAbs');
+traintestPosData = fullfile(arbabfolder, 'traintest', 'adCorrPos');
 
 % settings
 arbabsettings = fullfile('exp', 'experiments', 'arbabshiraniSettings.m');
 
 % summary
 settingFiles = {arbabsettings};
-data = {traintestData, looTrainData, looTestData, looAllData};
+data = {traintestOneICAData, looTrainData, looTestData, looAllData, traintestAbsData, traintestPosData};
 expname = 'exp_arbab_abs_oneICA';
 
 % running experiment
