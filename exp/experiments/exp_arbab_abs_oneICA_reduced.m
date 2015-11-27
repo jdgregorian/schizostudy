b4d85e61ca5af9697a471af61eeb853cadbc06c8
Zbynek-T-111 <jdgregorian@seznam.cz>
2015-11-27 14:22:28 +0100

    Test for arbabshirani reduced by Schneider added

---
+++ exp/experiments/exp_arbab_abs_oneICA_reduced.m
@@ -0,0 +1,27 @@
+% Experiment for running arbabshirani settings on reduced data with
+% absolute values of coefficients but without running ICASSO
+% plus traintest data with ICASSO
+
+% initialization
+expfolder = fullfile('exp', 'experiments');
+
+% datafiles
+arbabfolder = fullfile('data', 'arbabshirani');
+looTrainData = fullfile(arbabfolder, 'loo', 'adCorrAbs_oneICA_reduced', '80subj_training.mat');
+looTestData = fullfile(arbabfolder, 'loo', 'adCorrAbs_oneICA_reduced', '68subj_testing.mat');
+looAllData = fullfile(arbabfolder, 'loo', 'adCorrAbs_oneICA_reduced', '148subj_all.mat');
+traintestOneICAData = fullfile(arbabfolder, 'traintest', 'adCorrAbs_oneICA_reduced');
+
+% settings
+arbabsettings = fullfile('exp', 'experiments', 'arbabshiraniSettings.m');
+
+% summary
+settingFiles = {arbabsettings};
+data = {traintestOneICAData, looTrainData, looTestData, looAllData, traintestAbsData, traintestPosData};
+expname = 'exp_arbab_abs_oneICA';
+
+% running experiment
+runExperiment(settingFiles, data, expname)
+
+% final results listing
+listSettingsResults(fullfile(expfolder, expname));
