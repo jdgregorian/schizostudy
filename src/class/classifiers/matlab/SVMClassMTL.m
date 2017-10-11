64b7531bd35abbcc9b6e592d0da57be2fc567f52
Zbynek Pitra <jdgregorian@seznam.cz>
2017-10-11 16:06:06 +0200

    SVM classifier updated to new Matlab versions

--- src/class/classifiers/matlab/SVMClassMTL.m
+++ src/class/classifiers/matlab/SVMClassMTL.m
@@ -12,19 +12,42 @@ classdef SVMClassMTL < MatlabClassifier
     % constructor
       obj = obj@MatlabClassifier(settings);
       obj.method = 'svm';
+      if verLessThan('matlab', '9.2')
+        obj.settings.kernelfunc = defopts(obj.settings, 'kernelfunction', 'linear');
+        obj.settings.kernelfunc = defopts(obj.settings, 'kernel_function', obj.settings.kernelfunc);
+      else
+        obj.settings.kernelfunc = defopts(obj.settings, 'kernel_function', 'linear');
+        obj.settings.kernelfunc = defopts(obj.settings, 'kernelfunction', obj.settings.kernelfunc);
+        if strcmpi(obj.settings.kernelfunc, 'quadratic')
+          obj.settings.kernelfunc = 'polynomial';
+          obj.settings.polynomialorder = 2;
+        end
+      end
     end
     
     function obj = trainClassifier(obj, trainingData, trainingLabels)
     % training function
-      cellset = cellSettings(obj.settings, {'gridsearch', 'implementation', 'prior'});
-      obj.classifier = svmtrain(trainingData, trainingLabels, cellset{:});
-%       obj.classifier = fitcsvm(trainingData, trainingLabels, cellset{:});
+      cellset = cellSettings(obj.settings, ...
+                 {'gridsearch', 'implementation', 'prior', ...
+                  'kernel_function', 'kernelfunction', 'kernelfunc'});
+      if verLessThan('matlab', '9.2')
+        obj.classifier = svmtrain(trainingData, trainingLabels, ...
+                                  'kernel_function', obj.settings.kernelfunc, ...
+                                  cellset{:});
+      else
+        obj.classifier = fitcsvm(trainingData, trainingLabels, ...
+                                 'kernelfunction', obj.settings.kernelfunc, ...
+                                 cellset{:});
+      end
     end
     
     function y = predict(obj, testingData, ~, ~)
     % prediction using SVM
-     y = svmclassify(obj.classifier, testingData);
-%       y = predict(obj.classifier, testingData);
+      if verLessThan('matlab', '9.2')
+        y = svmclassify(obj.classifier, testingData);
+      else
+        y = predict(obj.classifier, testingData);
+      end
     end
     
   end
