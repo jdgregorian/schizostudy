e1acc2491ca56cd6fe0b6bc471e014d6a718d5c3
Zbynek Pitra <jdgregorian@seznam.cz>
2017-10-29 15:55:26 +0100

    Additional fcpaper settings added

---
+++ exp/settings/fcpaperSettings_add.m
@@ -0,0 +1,105 @@
+% Script for additional testing settings of classifiers used in FC paper 
+% (2017)
+%
+% Variables 'FCdata', 'filename', 'expfolder' and 'datamark' should be 
+% defined before run.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% initialization
+if ~exist('FCdata', 'var')
+  FCdata = fullfile('data', 'data_FC_180subjects.mat');
+end
+if ~exist('filename', 'var')
+  filename = 'fcpaperSettings';
+end
+if ~exist('expfolder', 'var')
+  expfolder = fullfile('exp', 'experiments');
+end 
+if ~exist('datamark', 'var')
+  datamark = '';
+else
+  datamark = ['_', datamark];
+end
+mkdir(expfolder, filename)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% SVM
+% linear - iter 15000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.options = statset('MaxIter', 15000);
+settings.note = 'Linear SVM. Maximum iterations 15000 (default).';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_iter15', datamark, '.mat']));
+
+%% linear - iter 20000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.options = statset('MaxIter', 20000);
+settings.note = 'Linear SVM. Maximum iterations 20000.';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_iter20', datamark, '.mat']));
+
+%% linear - iter 25000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.options = statset('MaxIter', 25000);
+settings.note = 'Linear SVM. Maximum iterations 25000.';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_iter25', datamark, '.mat']));
+
+%% linear - iter 30000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.options = statset('MaxIter', 30000);
+settings.note = 'Linear SVM. Maximum iterations 30000.';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_iter30', datamark, '.mat']));
+
+%% linear - autoscale 'off', iter 15000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.autoscale = false;
+settings.svm.options = statset('MaxIter', 15000);
+settings.note = 'Linear SVM. Autoscale ''off''. Maximum iterations 15000 (default).';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_noauto_iter15', datamark, '.mat']));
+
+%% linear - autoscale 'off', iter 20000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.autoscale = false;
+settings.svm.options = statset('MaxIter', 20000);
+settings.note = 'Linear SVM. Autoscale ''off''. Maximum iterations 20000.';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_noauto_iter20', datamark, '.mat']));
+
+%% linear - autoscale 'off', iter 25000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.autoscale = false;
+settings.svm.options = statset('MaxIter', 25000);
+settings.note = 'Linear SVM. Autoscale ''off''. Maximum iterations 25000.';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_noauto_iter25', datamark, '.mat']));
+
+%% linear - autoscale 'off', iter 30000
+clear settings
+
+settings.svm.kernel_function = 'linear';
+settings.svm.autoscale = false;
+settings.svm.options = statset('MaxIter', 30000);
+settings.note = 'Linear SVM. Autoscale ''off''. Maximum iterations 30000.';
+
+classifyFC(FCdata, 'svm', settings, fullfile(filename, ['svm_linear_noauto_iter30', datamark, '.mat']));
+
+%% final results listing
+
+listSettingsResults(fullfile(expfolder, filename));
